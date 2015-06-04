b3ffbbe6673d6dbdcffbff3dc41c0c6f938bcafc
Alan Thompson <cloojure@gmail.com>
2015-06-03 23:06:45 -0700

    misc

--- datomic/basic/src/basic/core.clj
+++ datomic/basic/src/basic/core.clj
@@ -478,6 +478,40 @@
 (let [db-since-data (d/since db-val-new data-tx-date)]
   (spyx (count (d/q communities-query db-since-data))))
 
+;-----------------------------------------------------------------------------
+
+(newline)
+(println "making :communities partition")
+@(d/transact conn [ {:db/id (d/tempid :db.part/db)
+                     :db/ident  :communities
+                     :db.install/_partition   :db.part/db} ] )
+
+(newline)
+(println "making Easton community")
+@(d/transact conn [ {:db/id (d/tempid :communities)
+                     :community/name "Easton"} ] )
+
+;get id for a community, use to transact data
+(newline)
+(def belltown-id-entity (d/q '[:find ?id
+                        :where [?id :community/name "belltown"] ]
+                        db-val-new ))
+(spyxx belltown-id-entity)
+(def belltown-id-dot (d/q '[:find ?id .
+                        :where [?id :community/name "belltown"] ]
+                      db-val-new ))
+(spyxx belltown-id-dot)
+
+(newline)
+(println "Adding 'free stuff' for belltown")
+@(d/transact conn [ {:db/id belltown-id-dot
+                     :community/category "free stuff"} ] )    ; map syntax
+(println "Retracting 'free stuff' for belltown")
+@(d/transact conn [ [:db/retract belltown-id-dot
+                     :community/category "free stuff"] ] )    ; tuple syntax
+
+
+
 (println "exiting")
 (System/exit 0)
 
