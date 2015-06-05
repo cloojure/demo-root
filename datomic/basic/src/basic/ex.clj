730adf4351dd57e9d60ff1ed4489ed85ae6f6dce
Alan Thompson <cloojure@gmail.com>
2015-06-05 10:31:09 -0700

    misc

--- datomic/basic/src/basic/ex.clj
+++ datomic/basic/src/basic/ex.clj
@@ -18,14 +18,42 @@
 
 (def data-tx
   [
-    {:db/id #db/id[:people -007]  :person/name      "James Bond"}
-    {:db/id #db/id[:people -007]  :person/ssn-usa   "123-45-6789"}
-    {:db/id #db/id[:people -007]  :person/ssn-uk    "123-45-6789"}
+    ; map-format transaction data: 
+    {:db/id #db/id[:people -007]                                        ; entity specification
+                                    :person/name      "James Bond"      ; multiple attribute/value pairs
+                                    :person/ssn-usa   "123-45-6789"     ; multiple attribute/value pairs
+                                    :person/ssn-uk    "123-45-6789"}    ; multiple attribute/value pairs
 
-    {:db/id #db/id[:people -666]  :person/name      "Mephistopheles"}
-    {:db/id #db/id[:people -666]  :person/ssn-hell  "123-45-6789"}
+  ; list-format transaction data
+  ; [<action>   <entity tmpid>      <attribute>        <value>  ]
+    [:db/add  #db/id[:people -666]  :person/name      "Mephistopheles"]
+    [:db/add  #db/id[:people -666]  :person/ssn-hell  "123-45-6789"]
+              ; [<partition> <tmpid>]
   ] )
 @(d/transact conn data-tx)
+
+; Since the name is :db.unique/value, we can use that to update our entities 
+@(d/transact conn [
+  ; add a new attribute
+  { :db/id                        #db/id[:db.part/db]
+    :db/ident                     :weapon/type
+    :db/valueType                 :db.type/keyword
+    :db/cardinality               :db.cardinality/many
+    :db.install/_attribute        :db.part/db }
+] )
+@(d/transact conn [
+  ; give James some weapons - map-format
+  { :db/id  [:person/name "James Bond"]        :weapon/type :weapon/gun    }
+  { :db/id  [:person/name "James Bond"]        :weapon/type :weapon/knife  }
+  { :db/id  [:person/name "James Bond"]        :weapon/type :weapon/guile  }
+    ; must use a separate maps, since cannot have multiple :weapon/type keys in one map
+
+  ; give the Devil his due - list-format
+  [ :db/add  [:person/name "Mephistopheles"]   :weapon/type :weapon/curse  ]
+  [ :db/add  [:person/name "Mephistopheles"]   :weapon/type :weapon/guile  ]
+    ; list format is always one "fact" (EAV) per list
+] )
+
 (def db-val (d/db conn))
 (spyxx db-val)
 
