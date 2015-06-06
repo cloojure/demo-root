3149befdaee755b5d82cf5b1558e114362e86582
Alan Thompson <cloojure@gmail.com>
2015-06-05 17:54:56 -0700

    Got tx-eid out for Honey Rider

--- datomic/basic/src/basic/ex.clj
+++ datomic/basic/src/basic/ex.clj
@@ -8,17 +8,70 @@
   (:gen-class))
 
 
-(def uri "datomic:mem://example")
+(def Eid Long)
 
+(def uri "datomic:mem://example")
 (d/create-database uri)
 (def conn (d/connect uri))
-
 (def schema-tx (read-string (slurp "ex-schema.edn")))
 @(d/transact conn schema-tx)
 
+(defn show-db [db-val]
+  (println "-----------------------------------------------------------------------------")
+    (s/def res-1 :- #{ [Eid] }
+      (into #{}
+        (d/q '{:find [?c]
+               :where [ [?c :person/name]
+                      ]
+              }
+             db-val )))
+  ; (spyx (count res-1))
+  ; (spyxx res-1)
+
+    (doseq [it res-1]
+      (let [eid     (first it)
+            entity  (d/touch (d/entity db-val eid))
+            map-val (into (sorted-map) entity)
+           ]
+        (newline)
+      ; (spyxx entity)
+        (pprint map-val)
+      )))
+
+
+(defn show-db-tx [db-val]
+  (newline)
+  (println "-----------------------------------------------------------------------------")
+  (println "Database Transactions")
+  (let [
+        res-1     (into (sorted-set)
+                    (d/q '{:find [?c]
+                           :where [ [?c :db/txInstant]
+                                  ]
+                          }
+                         db-val ))
+      ; _ (spyx (count res-1))
+      ; _ (spyxx res-1)
+
+        res-2     (for [it res-1]
+                    (let [eid     (first it)
+                          entity  (d/touch (d/entity db-val eid))
+                          map-val (into (sorted-map) entity) ]
+                      map-val ))
+      ; _ (spyx (count res-2))
+      ; _ (spyxx res-2)
+
+        res-4     (into (sorted-set-by #(.compareTo (:db/txInstant %1) (:db/txInstant %2) ))
+                        res-2)
+      ; _ (spyxx (count res-4))
+      ; _ (spyxx res-4)
+  ]
+    (doseq [it res-4]
+      (pprint it))))
+
 (def data-tx
   [
-    ; map-format transaction data: 
+    ; map-format transaction data:
     {:db/id #db/id[:people -007]                                        ; entity specification
                                     :person/name      "James Bond"      ; multiple attribute/value pairs
                                     :person/ssn-usa   "123-45-6789"     ; multiple attribute/value pairs
@@ -42,12 +95,12 @@
     :db.install/_attribute  :db.part/db }
 ] )
 
-; Since the name is :db.unique/value, we can use that to update our entities 
+; Since the name is :db.unique/value, we can use that to update our entities
 @(d/transact conn [
   ; give James some weapons - map-format
-  { :db/id  [:person/name "James Bond"]        
+  { :db/id  [:person/name "James Bond"]
         :weapon/type  #{:weapon/gun :weapon/knife :weapon/guile}  }
-        ; must use a a set for multiple values of a single attr :weapon/type 
+        ; must use a a set for multiple values of a single attr :weapon/type
 
   ; give the Devil his due - list-format (using a set for attr-value does not work here)
   [ :db/add  [:person/name "Mephistopheles"]   :weapon/type :weapon/curse  ]
@@ -61,26 +114,52 @@
   { :db/id  [:person/name "James Bond"]  :person/name "Bond, James Bond" }
 ] )
 
-(def db-val (d/db conn))
-(spyxx db-val)
+(newline) (println "initial db")
+(show-db (d/db conn))
 
-(def res-1 (d/q '{:find [?c]
-                  :where [ [?c :person/name] 
-                         ] 
-                 }
-                db-val ))
-(spyx (count res-1))
-(spyxx res-1)
+; James has dropped his knife...
+@(d/transact conn [
+  [:db/retract  [:person/name "Bond, James Bond"] :weapon/type  :weapon/knife]
+] )
+(newline) (println "dropped knife")
+(show-db (d/db conn))
 
-(doseq [it res-1]
-  (let [eid     (spyxx (first it))
-        entity  (d/touch (d/entity db-val eid)) 
-        map-val (into (sorted-map) entity)
-       ]
-    (newline)
-    (spyxx entity)
-    (pprint map-val)
-  ))
+; Add a new weapon type
+@(d/transact conn [
+  [:db/add  #db/id[:db.part/user]  :weapon/type  :weapon/feminine-charm]
+] )
+
+; Add Honey Rider & annotate the tx
+(let [tx-tmpid            (d/tempid :db.part/tx)
+        _ (spyxx tx-tmpid)
+      honey-rider-tmpid   (d/tempid :people)
+      tx-data             [ { :db/id  honey-rider-tmpid
+                              :person/name    "Honey Rider"
+                              :weapon/type    :weapon/feminine-charm }
+                            { :db/id  tx-tmpid
+                              :data/src "Dr. No" }
+                          ]
+      tx-result           @(d/transact conn tx-data)
+      ; _ (spyxx tx-result)
+      datoms    (safe-> :tx-data  tx-result)
+      tempids   (safe-> :tempids  tx-result)
+      tx-eid    (-> tx-tmpid tempids)
+     ]
+  (newline)
+  (println "Tx Data:")
+  (doseq [it datoms] (pprint it))
+  (newline)
+  (println "Temp IDs:")
+  (spyxx tempids)
+  (println "tx-eid" tx-eid)
+)
+(newline) (println "added beauty")
+(show-db (d/db conn))
+
+(show-db-tx (d/db conn))
+(println "exit")
+(System/exit 1)
+
 
 (defn -main []
   (newline)
