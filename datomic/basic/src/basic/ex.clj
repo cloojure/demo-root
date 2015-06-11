3a58fc74d498f52e19b57c2270a88691b39773ea
Alan Thompson <cloojure@gmail.com>
2015-06-10 22:35:25 -0700

    misc

--- datomic/basic/src/basic/ex.clj
+++ datomic/basic/src/basic/ex.clj
@@ -34,9 +34,8 @@
   (println "-----------------------------------------------------------------------------")
     (s/def res-1 :- #{ [Eid] }
       (into #{}
-        (d/q '{:find [?c]
-               :where [ [?c :person/name]
-                      ]
+        (d/q '{:find  [?e]
+               :where [ [?e :person/name] ]
               }
              db-val )))
     (doseq [it res-1]
@@ -67,25 +66,31 @@
 ; load 2 antagonists into the db
 @(d/transact conn [
   ; map-format transaction data (the "add" command is implicit)
-  { :db/id            #db/id[:people -007]      ; entity specification
+  { :db/id            (d/tempid :people -007)   ; entity specification
     :person/name      "James Bond"              ; multiple attribute/value pairs
     :person/ssn-usa   "123-45-6789"             ; multiple attribute/value pairs
     :person/ssn-uk    "123-45-6789"}            ; multiple attribute/value pairs
 
   ; list-format transaction data
   ; [<action>   <entity tmpid>      <attribute>        <value>  ]
-    [:db/add  #db/id[:people -666]  :person/name      "Mephistopheles"]
-    [:db/add  #db/id[:people -666]  :person/ssn-hell  "123-45-6789"]
+    [:db/add  (d/tempid :people -666)  :person/name      "Mephistopheles"]
+    [:db/add  (d/tempid :people -666)  :person/ssn-hell  "123-45-6789"]
               ; [<partition> <tmpid>]
               ; Note that <partition> could be namespaced like :beings.sentient/people
 ] )
 
 ; Add a new attribute. This must be done in a separate tx before we attempt to use the new attribute.
 @(d/transact conn [
-  { :db/id                  #db/id[:db.part/db]
+  { :db/id                  (d/tempid :db.part/db)
     :db/ident               :weapon/type
     :db/valueType           :db.type/keyword
     :db/cardinality         :db.cardinality/many
+    :db.install/_attribute  :db.part/db }
+
+  { :db/id                  (d/tempid :db.part/db)
+    :db/ident               :favorite-weapon    ; ident doesn't need a namespace
+    :db/valueType           :db.type/keyword
+    :db/cardinality         :db.cardinality/one
     :db.install/_attribute  :db.part/db }
 ] )
 
@@ -93,14 +98,43 @@
 @(d/transact conn [
   ; give James some weapons - map-format
   { :db/id  [:person/name "James Bond"]
-        :weapon/type  #{:weapon/gun :weapon/knife :weapon/guile}  }
+        :weapon/type        #{:weapon/gun :weapon/knife :weapon/guile}  
+        :favorite-weapon    :weapon/gun
+   }
         ; must use a a set for multiple values of a single attr :weapon/type
 
   ; give the Devil his due - list-format (using a set for attr-value does not work here)
-  [ :db/add  [:person/name "Mephistopheles"]   :weapon/type :weapon/curse  ]
-  [ :db/add  [:person/name "Mephistopheles"]   :weapon/type :weapon/guile  ]
+  [ :db/add  [:person/name "Mephistopheles"]   :weapon/type       :weapon/curse ]
+  [ :db/add  [:person/name "Mephistopheles"]   :weapon/type       :weapon/guile ]
+  [ :db/add  [:person/name "Mephistopheles"]   :favorite-weapon   :weapon/curse ]
     ; list format is always one "fact" (EAV) per list
 ] )
+
+(newline) (println "initial db")
+(show-db (d/db conn))
+
+(let [james-raw       (d/q '{:find [?e  ]  :where [ [?e :person/name "James Bond"] ] }  (d/db conn))
+      james-scalar    (d/q '{:find [?e .]  :where [ [?e :person/name "James Bond"] ] }  (d/db conn))
+      james-flat      (flatten (into [] james-raw))
+  ]
+    (spyxx james-raw)
+    (spyxx james-scalar)
+    (spyxx james-flat))
+
+(let [weapon-holders-1    (d/q '{:find [?e  ]  :where [ [?e :weapon/type] ] }  (d/db conn))
+      weapon-holders-2    (d/q '{:find [?e .]  :where [ [?e :weapon/type] ] }  (d/db conn))
+      weapon-holders-3    (d/q '{:find [[?e]]  :where [ [?e :weapon/type] ] }  (d/db conn))
+  ]
+    (spyxx weapon-holders-1)
+    (spyxx weapon-holders-2)
+    (spyxx weapon-holders-3)
+)
+
+
+(newline) (println "James 'e' value:")
+(def james-eid (s/validate Eid
+  (ffirst (d/q '{:find [?e]  :where [ [?e :person/name "James Bond"] ] }  (d/db conn)))))
+(spyxx james-eid)
 
 ; Updated James' name. Note that we can use the current value of name for lookup, then add in the
 ; new name w/o conflict.
@@ -108,19 +142,31 @@
   { :db/id  [:person/name "James Bond"]  :person/name "Bond, James Bond" }
 ] )
 
-(newline) (println "initial db")
-(show-db (d/db conn))
-
 ; James has dropped his knife...
 @(d/transact conn [
   [:db/retract  [:person/name "Bond, James Bond"] :weapon/type  :weapon/knife]
 ] )
-(newline) (println "dropped knife")
+(newline) (println "James dropped knife + new name")
 (show-db (d/db conn))
+
+; James changes his favorite weapon
+(let [tx-result   @(d/transact conn [
+                    { :db/id  james-eid  :favorite-weapon :weapon/guile }
+                  ] )
+]
+  (newline) (println "James changes his favorite weapon - db-before:")
+  (show-db (:db-before tx-result))
+  (newline) (println "James changes his favorite weapon - db-after:")
+  (show-db (:db-after  tx-result))
+  (newline) (println "James changes his favorite weapon - datoms:")
+  (pprint  (:tx-data   tx-result))
+  (newline) (println "James changes his favorite weapon - tempids")
+  (pprint  (:tempids   tx-result))
+)
 
 ; Add a new weapon type
 @(d/transact conn [
-  [:db/add  #db/id[:db.part/user]  :weapon/type  :weapon/feminine-charm]
+  [:db/add  (d/tempid :db.part/user)  :weapon/type  :weapon/feminine-charm]
 ] )
 
 (s/defn txid  :- TxResult
