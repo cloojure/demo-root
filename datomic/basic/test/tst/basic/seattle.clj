f7b6fe0ef3a97d09fc3354114ea9da6f57ba4265
Alan Thompson <cloojure@gmail.com>
2015-06-17 10:24:33 -0700

    misc

--- datomic/basic/test/tst/basic/seattle.clj
+++ datomic/basic/test/tst/basic/seattle.clj
@@ -100,10 +100,10 @@
                                entity-maps )
         results           (take 5 (sort comm-nbr-names))
     ]
-      (is (= results  [ ["15th Ave Community"                 "Capitol Hill"              ]
-                        ["Admiral Neighborhood Association"   "Admiral (West Seattle)"    ]
-                        ["Alki News"                          "Alki"                      ]
-                        ["Alki News/Alki Community Council"   "Alki"                      ]
-                        ["All About Belltown"                 "Belltown"                  ] ] ))
+      (is (= results  [ ["15th Ave Community"                 "Capitol Hill"            ]
+                        ["Admiral Neighborhood Association"   "Admiral (West Seattle)"  ]
+                        ["Alki News"                          "Alki"                    ]
+                        ["Alki News/Alki Community Council"   "Alki"                    ]
+                        ["All About Belltown"                 "Belltown"                ] ] ))
   ))
 
