42ab9c51205c09ebf3282bc03062d20b8a58da2e
Alan Thompson <cloojure@gmail.com>
2015-06-17 00:15:26 -0700

    misc

--- datomic/basic/test/tst/basic/seattle.clj
+++ datomic/basic/test/tst/basic/seattle.clj
@@ -56,32 +56,30 @@
                         (take 3 it))
 
         ; The value for :community/neighborhood is another entity (datomic.query.EntityMap) like {:db/id <eid>},
-        ; where the <eid> is volatile.  Replace this degenerate & volatile value with a plain clojure map like
-        ; { :neighborhood/name <some-string> }
+        ; where the <eid> is volatile.  Replace this degenerate & volatile value with a dummy value
+        ; in a plain clojure map.
         sample-comm-nbr  (:community/neighborhood (first entity-maps))
         _ (is (= datomic.query.EntityMap (class sample-comm-nbr)))
-        first-3   (map #(assoc % :community/neighborhood 
-                                   {:neighborhood/name (-> % :community/neighborhood :neighborhood/name) } )
-                       first-3)
+        first-3   (map #(assoc % :community/neighborhood {:db/id -1}) first-3)
     ]
       (is (=  first-3
               [ {:community/category #{"15th avenue residents"},
                  :community/name "15th Ave Community",
-                 :community/neighborhood {:neighborhood/name "Capitol Hill"}
+                 :community/neighborhood {:db/id -1}
                  :community/orgtype :community.orgtype/community,
                  :community/type #{:community.type/email-list},
                  :community/url "http://groups.yahoo.com/group/15thAve_Community/"}
 
                 {:community/category #{"neighborhood association"},
                  :community/name "Admiral Neighborhood Association",
-                 :community/neighborhood {:neighborhood/name "Admiral (West Seattle)"}
+                 :community/neighborhood {:db/id -1}
                  :community/orgtype :community.orgtype/community,
                  :community/type #{:community.type/email-list},
                  :community/url "http://groups.yahoo.com/group/AdmiralNeighborhood/"}
 
                 {:community/category #{"members of the Alki Community Council and residents of the Alki Beach neighborhood"},
                  :community/name "Alki News",
-                 :community/neighborhood {:neighborhood/name "Alki"}
+                 :community/neighborhood {:db/id -1}
                  :community/orgtype :community.orgtype/community,
                  :community/type #{:community.type/email-list},
                  :community/url "http://groups.yahoo.com/group/alkibeachcommunity/"} ] ))
