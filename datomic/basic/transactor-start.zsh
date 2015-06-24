9815a54d98eeb278303fe8f1d5cf8a969b01083d
Alan Thompson <cloojure@gmail.com>
2015-06-23 17:48:08 -0700

    misc

--- datomic/basic/transactor-start.zsh
+++ datomic/basic/transactor-start.zsh
@@ -1,5 +1,10 @@
 #!/bin/zsh -v
+echo ""
+echo "  Usage:  sudo -u datomic ./transactor-start.zsh "
+echo ""
+
 export XMX=-Xmx4g
 export XMS=-Xms4g
 cd /opt/datomic
 ./bin/transactor ./dev-transactor-template.properties
+
